047db9915de1bcd9bb5ebb4b3c59c5c13255f606
kp-devs <111303123+kp-labs-x@users.noreply.github.com>
2026-08-19 14:48:01 -0700

    update test coverage

--- src/tests/tests.test.ts
+++ src/tests/tests.test.ts
@@ -1,9 +1,20 @@
 import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth";
 
 const person = {
   isActive: true,
   age: 32,
 };
+
+test("getAPIKey returns the API key, () => {
+  const headers = {
+    authorization: "Apikey banana789",
+  };
+
+  const result = getAPIKey(headers);
+
+  expect(result).toBe("banana789);
+});
 
 describe("person", () => {
   test("person is defined", () => {
